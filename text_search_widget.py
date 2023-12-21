f0d076dcc0476e37696c84277cb4e3a9b31333f5
chokasapps <154495519+chokasapps@users.noreply.github.com>
2023-12-21 11:29:09 +0500

    first commit

---
+++ text_search_widget.py
@@ -0,0 +1,98 @@
+from langchain.agents import ConversationalChatAgent, AgentExecutor
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
+from langchain.document_loaders import DirectoryLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+import streamlit as st
+from langchain.agents.agent_toolkits import create_retriever_tool
+
+DOCS_FOLDER = "D:\\edo_este\\uploads"
+
+st.set_page_config(
+    page_title="Using Streamlit and Pinecone, build a simple LLM app POC - chat with user uploaded PDFs"
+)
+st.title(
+    "Using Streamlit and Pinecone, build a simple LLM app POC - chat with user uploaded PDFs"
+)
+
+openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+
+
+@st.cache_resource  #
+def get_executer(_memory):
+    llm = ChatOpenAI(
+        model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, streaming=True
+    )
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    loader = DirectoryLoader(
+        DOCS_FOLDER,
+        glob="**/*.pdf",
+        loader_cls=PyPDFLoader,
+        show_progress=True,
+    )
+    docs = loader.load()
+    vectorstore = FAISS.from_documents(documents=docs, embedding=embeddings)
+    retriever = vectorstore.as_retriever()
+    retriever_tool = create_retriever_tool(
+        retriever,
+        "search_state_of_union",
+        "Searches and returns documents regarding the state-of-the-union.",
+    )
+    tools = [retriever_tool]
+    chat_agent = ConversationalChatAgent.from_llm_and_tools(llm=llm, tools=tools)
+    executor = AgentExecutor.from_agent_and_tools(
+        agent=chat_agent,
+        tools=tools,
+        memory=_memory,
+        return_intermediate_steps=True,
+        handle_parsing_errors=True,
+    )
+    return executor
+
+
+msgs = StreamlitChatMessageHistory()
+memory = ConversationBufferMemory(
+    chat_memory=msgs,
+    return_messages=True,
+    memory_key="chat_history",
+    output_key="output",
+)
+if len(msgs.messages) == 0 or st.sidebar.button("Reset chat history"):
+    msgs.clear()
+    msgs.add_ai_message("How can I help you?")
+    st.session_state.steps = {}
+
+avatars = {"human": "user", "ai": "assistant"}
+for idx, msg in enumerate(msgs.messages):
+    with st.chat_message(avatars[msg.type]):
+        # Render intermediate steps if any were saved
+        for step in st.session_state.steps.get(str(idx), []):
+            if step[0].tool == "_Exception":
+                continue
+            with st.status(
+                f"**{step[0].tool}**: {step[0].tool_input}", state="complete"
+            ):
+                st.write(step[0].log)
+                st.write(step[1])
+        st.write(msg.content)
+
+if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?"):
+    st.chat_message("user").write(prompt)
+
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    executor = get_executer(memory)
+
+    with st.chat_message("assistant"):
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        response = executor(prompt, callbacks=[st_cb])
+        st.write(response["output"])
+        st.session_state.steps[str(len(msgs.messages) - 1)] = response[
+            "intermediate_steps"
+        ]
